509d14e7d9329a972110fa44f85b89720dd14886
zhangdx <zdx3987@163.com>
2025-08-27 15:31:53 +0800

    fix:增加水印组件

--- src/api/oauthApi.ts
+++ src/api/oauthApi.ts
@@ -1,7 +1,12 @@
 import type {LoginForm} from "../type/LoginForm.ts";
 import axios, {type ApiResponse} from "./axios.ts";
 import {genApiResponse} from "../utils/api-util.ts";
+import {AuthUserInfo} from "../type/AuthUserInfo.ts";
 
 export function login(loginForm: LoginForm): Promise<ApiResponse<void>> {
     return genApiResponse(axios.post('/api/login', loginForm))
+}
+
+export function getCurrUser(): Promise<ApiResponse<AuthUserInfo>> {
+    return genApiResponse(axios.get('/api/user/curruser'))
 }
---
+++ src/type/AuthUserInfo.ts
@@ -0,0 +1,10 @@
+export class AuthUserInfo {
+    userId: number;
+    username: string;
+    nickname: string;
+    avatar: string;
+    email: string;
+    tel: string;
+    introduce: string;
+    createTime: Date;
+}
